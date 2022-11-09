26fcd34743d7ff56b8f81817c2d67011e8d26116
Daniel-Sanchez-111 <dan.2001@hotmail.com>
2022-11-08 22:02:08 -0700

    index

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+
+<body>
+
+    <div style="text-align:center;">
+
+        <h2>
+            Universidad Autonoma de Baja California Sur
+        </h2>
+
+        <h3>Departamento Academico de Sistemas Computacionales</h3>
+        
+        <h3>Ingenieria en Desarrollo de Software</h3>
+        
+        <h3>7mo. Semestre TM.</h3>
+        
+        <h3>Sistemas Operativos</h3>
+        
+        <h3>M.T.I Nelson Israel Higuera Castillo</h3>
+        
+        <h3>Daniel Eduardo Sánchez Carrillo</h3>
+
+        <li>
+            <ul><a href="gantt.php">Diagrama de Gantt</a></ul>
+        </li>
+
+    </div>
+
+</body>
+
+</html>
